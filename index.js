ad89dd1532364ff96be4ca6104806bb77db637fe
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-24 21:02:07 -0900

    Edit object names  using convetional naming

--- index.js
+++ index.js
@@ -4,21 +4,25 @@
 /*The items sold at the tuck shop will be represented by objects
 which have properties: name, price and quantity for each product*/
 
-const coke={
+const COKE={
     name: "Coca-cola",
     price: 10.00,
     quantity: 100
 };
 
-const pie={
+const PIE={
     name: "Pie",
     price: 15.00,
     quantity: 50
 };
 
-const lollipop={
+const LOLLIPOP={
     name: "Lollipop",
     price: 1.50,
     quantity: 500
 };
+
+const SNACKS={
+
+}
 
