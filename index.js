257c90afb147b95905521285cb48e121241c44db
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-24 21:07:01 -0900

    Second row of object for products added

--- index.js
+++ index.js
@@ -7,22 +7,36 @@ which have properties: name, price and quantity for each product*/
 const COKE={
     name: "Coca-cola",
     price: 10.00,
-    quantity: 100
+    quantity: 10
 };
 
 const PIE={
     name: "Pie",
     price: 15.00,
-    quantity: 50
+    quantity: 5
 };
 
 const LOLLIPOP={
     name: "Lollipop",
     price: 1.50,
-    quantity: 500
+    quantity: 10
 };
 
 const SNACKS={
+    name: "Snacks",
+    price: 5.00,
+    quantity: 10
+};
 
-}
+const JUICE={
+    name: "Juice",
+    price: 5.00,
+    quantity: 10
+};
+
+const SANDWICH={
+    name: "Sandwich",
+    price: 15.00,
+    quantity: 3
+};
 
