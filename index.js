b1332a6f1207cc5bacc6f5777ca8479cade29cd1
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-24 20:58:59 -0900

    index.js added with three objects defined

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+/*The items sold at the tuck shop will be represented by objects
+which have properties: name, price and quantity for each product*/
+
+const coke={
+    name: "Coca-cola",
+    price: 10.00,
+    quantity: 100
+};
+
+const pie={
+    name: "Pie",
+    price: 15.00,
+    quantity: 50
+};
+
+const lollipop={
+    name: "Lollipop",
+    price: 1.50,
+    quantity: 500
+};
+
