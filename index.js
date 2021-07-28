4a6fd37cd3ecfc0c8320184acfa7d2bb7f378017
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-27 19:40:04 -0900

    The checkout button sets number of items to zero and total cost also zero

--- index.js
+++ index.js
@@ -160,9 +160,12 @@ set items in list to zero and total to zero.*/
 CHECKOUT_BUTTON.onclick = ()=>{
     //clear the list if items are added
     if(numberOfItems > 0){
-
+        //set cost to zero
         totalCost = 0;
+        TOTAL.value = totalCost;
+        //number of items set to zero
         numberOfItems = 0;
+        VIEW_CART.innerHTML = numberOfItems;
         
     }
 }
